619921348fd37f8c5263c0210c804a2135f6f553
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2017-09-08 11:29:14 -0700

    Adding EmbeddedServer startup to help in debugging

---
+++ src/test/java/org/eclipse/jetty/demo/EmbeddedServer.java
@@ -0,0 +1,36 @@
+package org.eclipse.jetty.demo;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
+import org.eclipse.jetty.util.resource.PathResource;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class EmbeddedServer
+{
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        HandlerCollection handlers = new HandlerCollection();
+
+        Configuration.ClassList.setServerDefault(server)
+                .addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration",
+                        "org.eclipse.jetty.annotations.AnnotationConfiguration");
+
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        webapp.setBaseResource(new PathResource(MavenTestingUtils.getProjectDirPath("src/main/webapp")));
+        webapp.setExtraClasspath(MavenTestingUtils.getTargetPath("classes").toRealPath().toString());
+
+        handlers.addHandler(webapp);
+        handlers.addHandler(new DefaultHandler());
+
+        server.setHandler(handlers);
+
+        // server.setDumpAfterStart(true);
+        server.start();
+        server.join();
+    }
+}
